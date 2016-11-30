df5ecc87d2ab77fb24ffa3bfcf17a5b59c6ea455
AprilWong <aprilwang.zju@gmail.com>
2016-11-29 22:08:42 -0800

    add piano vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <piano></piano>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
+import Piano from './components/Piano'
 export default {
   name: 'app',
   components: {
-    Hello
+    Piano
   }
 }
 </script>
