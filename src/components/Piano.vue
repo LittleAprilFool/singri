a369d7efb49ec06ec422e63fbd4a687727fdc557
AprilWong <aprilwang.zju@gmail.com>
2016-12-04 14:15:19 -0800

    midi keyboard

--- src/components/Piano.vue
+++ src/components/Piano.vue
@@ -1,24 +1,36 @@
 <template lang="jade">
 div
+  button.test(v-on:click="buttonon")
+  - start_note = 60
+  - end_note = 91
   div.wrapout
     div.wrap
       div.gwhite
-        - for(var i = 0; i < 14; i++)
-          div.container.note.wnote(id= 'wk-' + i)
+        - for(var i = start_note; i <= end_note; i++)
+          - if([1, 3, 6, 8, 10].indexOf(i%12) == -1)
+            div.container.note.wnote(id='p'+i)
       div.gblack
-        - for(var i = 0; i < 13; i++)
-          div.container.note.bnote(id= 'bk-' + i)
+        - for(var i = start_note + 1; i <= end_note; i++)
+          - if([0, 1, 3, 4, 6, 8, 10].indexOf(i%12) != -1)
+            div.container.note.bnote(id='p'+i)
   div.wrapout
     div.wrap
       div.gwhite
-        - for(var i = 0; i < 14; i++)
-          div.container.key.wkey(id= 'wk-' + i, v-on:click="play($event)")
+        - for(var i = start_note; i <= end_note; i++)
+          - if([1, 3, 6, 8, 10].indexOf(i%12) == -1)
+            div.container.key.wkey(id='p'+i, v-on:click="play($event)")
       div.gblack
-        - for(var i = 0; i < 13; i++)
-          div.container.key.bkey(id= 'bk-' + i, v-on:click="play($event)")
+        - for(var i = start_note + 1; i <= end_note; i++)
+          - if([1, 3, 6, 8, 10].indexOf(i%12) != -1)
+            div.container.key.bkey(id='p'+i, v-on:click="play($event)")
+          - else
+            - if([0, 4].indexOf(i%12) != -1)
+              div.container.key.bkey(id='none')
   </button>
 </template>
 <script>
+let delay = 0 // play one note every quarter second
+let velocity = 127 // how hard the note hits
 export default {
   name: 'hello',
   data () {
@@ -27,26 +39,26 @@ export default {
     }
   },
   methods: {
+    buttonon: function () {
+      var note = 50 // the MIDI note
+      MIDI.noteOn(0, note, velocity, delay)
+      MIDI.noteOff(0, note, delay + 0.75)
+    },
     play: function (event) {
-      var sound = document.createElement('audio')
-      sound.setAttribute('src', '/static/' + event.target.id + '.mp3')
-      event.target.appendChild(sound)
-      event.target.classList.add('keydown')
-      sound.play()
+      var note = event.target.id.substring(1, event.target.id.length)
+      MIDI.noteOn(0, note, velocity, delay)
+      MIDI.noteOff(0, note, delay + 0.75)
       var container = document.querySelector('.note#' + event.target.id)
-      var note = document.createElement('div')
+      var littlenote = document.createElement('div')
       var text = ['♬', '♪', '♩', '♭', '♪', '๑', '❀', '☃', '☂', '❤']
-      // note.textContent = text[Math.floor(Math.random() * 10)]
-      note.textContent = text[1]
-      note.className = 'littlenote'
-      note.style['color'] = '#' + Math.floor(Math.random() * 16777215).toString(16)
-      container.appendChild(note)
-      sound.onended = function () {
-        this.remove()
-        event.target.classList.remove('keydown')
-        note.remove()
-        // note.classList.remove('dance')
-      }
+      littlenote.textContent = text[Math.floor(Math.random() * 10)]
+      littlenote.textContent = text[1]
+      littlenote.className = 'littlenote'
+      littlenote.style['color'] = '#' + Math.floor(Math.random() * 16777215).toString(16)
+      container.appendChild(littlenote)
+      setTimeout(function () {
+        littlenote.remove()
+      }, 4000)
     }
   }
 }
@@ -80,7 +92,7 @@ h2 {
   border-bottom-right-radius: 10px;
 }
 .note {
-  border: transparent;
+  border: solid 1px transparent;
   height: 200px;
 }
 .wkey {
@@ -120,7 +132,7 @@ h2 {
   position: absolute;
   left: 36px;
 }
-#bk-2, #bk-6, #bk-9 {
+#none{
   visibility:hidden;
 }
 .wrapout {
@@ -132,7 +144,7 @@ h2 {
     float: left;
     position: relative;
     left: -50%;
-    width: 780px;
+    width: 1000px;
     height: 200px;
 }
 
@@ -146,6 +158,7 @@ h2 {
   font-size: 2em;
   position: absolute;
   padding-left: 15px;
+  opacity: 0;
   animation-name: dance;
   animation-duration: 2.5s;
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import Vue from 'vue'
 import App from './App'
 
+MIDI.loadPlugin({
+  soundfontUrl: 'static/midi/soundfont/',
+  instrument: 'acoustic_grand_piano',
+  onprogress: function (state, progress) {
+    console.log(state, progress)
+  },
+  onsuccess: function () {
+    MIDI.setVolume(0, 127)
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
